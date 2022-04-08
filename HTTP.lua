79161d411efd0ade6b91e1bf8371c145a179ca93
clxyify <98536033+clxyify@users.noreply.github.com>
2022-04-08 10:11:44 -0400

    Update HTTP.lua

--- HTTP.lua
+++ HTTP.lua
@@ -1,12 +1,31 @@
-function HTTP_execute(site)
- wait(0.10)
+ 
+if HttpGet then
+    warn("my man you dont have httpget.")
+    else
+    wait(0.05)
+end
+
+if HTTP_execute then --if your using this for the first time, run the whole script
+ HTTP_execute("")--raw script data link (make sure it starts with https://)
+ else
+ function HTTP_execute(site)
  print("receiving http")
  loadstring(game:HttpGet(site, true))()
  print("http executed")
  end
- 
-if HttpGet then
-    warn("can't recieve! function doesn't exist!")
-    else
-    --pass
 end
+
+--for running URLs the second time
+HTTP_execute("")--url data here
+
+
+
+
+
+
+
+
+
+
+
+--made by speed softwares (speedsterkawaii aka clxify)
